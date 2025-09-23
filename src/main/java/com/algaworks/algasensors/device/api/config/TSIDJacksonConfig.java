3ba0e9d39867cea377796f4692d3d6db46d9dafa
Marcos Vassoler <marcosvassoler@finchsolucoes.com.br>
2025-09-23 08:38:47 -0300

    Commit com implementações iniciais do projeto do especialista microsserviços.

---
+++ src/main/java/com/algaworks/algasensors/device/api/config/TSIDJacksonConfig.java
@@ -0,0 +1,20 @@
+package com.algaworks.algasensors.device.api.config;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import io.hypersistence.tsid.TSID;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TSIDJacksonConfig {
+
+
+    @Bean
+    public Module tsidModule() {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(TSID.class, new TSIDToStringSerialize());
+        return module;
+    }
+
+}
